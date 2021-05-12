ba753a590a255a6913eaf4c83a718b28606b1db3
beinganushasan <beinganushasan@gmail.com>
2021-05-12 21:27:29 +0545

    frontend vue project added

---
+++ src/frontend/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+  Hello From Test
+</div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  methods: {
+    name() {
+      
+    }
+  },
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
